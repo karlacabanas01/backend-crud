4432585e2c89f5958748ebccea4ad2a7b96c3574
ext_jcartolorza <juancarlos@kranio.io>
2025-03-11 16:42:33 -0300

    update docker compose and setup guide

---
+++ middleware/errorHandler.js
@@ -0,0 +1,13 @@
+const errorHandler = (err, req, res, next) => {
+  console.log('❌ Error:', err)
+
+  const statusCode = err.statusCode || 500
+  const msg = err.message || 'Error interno del servidor'
+
+  res.status(statusCode).json({
+    success: false,
+    msg,
+    error: err.error || 'Error interno del servidor'
+  })
+}
+module.exports = { errorHandler }
